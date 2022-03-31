5ef6526c9754979cfc8c75e88eb5f9e7310ecc9c
dgh81 <danielguldberg@gmail.com>
2022-03-31 07:57:34 +0200

    employee and customer classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,29 +6,17 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) throws SQLException, FileNotFoundException {
+
         mysql msql = mysql.getInstance();
 
         User user1 = msql.getUserFromSQL("danielguldberg@gmail.com", "1234", "Customers");
-        System.out.println(user1.getEmail());
-
         User user2 = msql.getUserFromSQL("ragnar@vikings.dk", "4321", "Employees");
-        System.out.println(user2.getEmail());
 
-/*        //TODO gør generisk og flyt ud af main:
-        if(user.getClass().getName().equalsIgnoreCase("com.company.Customer")) {
-            System.out.println("This user is a Customer");
-        } else {
-            System.out.println("This user is an Employee");
-        }*/
         System.out.println(user1.getName() + " is of user subclass type " + user1.getUserSubClass(user1));
         System.out.println(user2.getName() + " is of user subclass type " + user2.getUserSubClass(user2));
 
         msql.printSQLTable("Customers");
         msql.printSQLTable("Employees");
-
-/*
-        User user = mysql.getUserFromSQL("danielguldberg@gmail.com", "1234", "Customers");
-        System.out.println(user.getEmail());*/
 
     }
 }
